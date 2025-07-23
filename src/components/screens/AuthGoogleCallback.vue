d0a354bcf91ff95759dd16a65c71653516b3c5cb
GoofyGoof <39170099+VyacheslavPridchin@users.noreply.github.com>
2025-07-23 07:05:40 +0300

    upd & build

--- src/components/screens/AuthGoogleCallback.vue
+++ src/components/screens/AuthGoogleCallback.vue
@@ -15,21 +15,36 @@ const router = useRouter()
 // Handle OAuth callback
 onMounted(async () => {
   const hash = window.location.hash
-  if (!hash) return
+  console.log(hash)
+  if (!hash) {
+    await router.push('/story/setup')
+    return
+  }
 
   const params = new URLSearchParams(hash.slice(1))
   const idToken = params.get('id_token')
-  if (!idToken) return
+  console.log(idToken)
+  if (!idToken) {
+    await router.push('/story/setup')
+    return
+  }
 
   try {
+    const { access_token, refresh_token } = await googleRegister({ token: idToken })
+    localStorage.setItem('access_token', access_token)
+    localStorage.setItem('refresh_token', refresh_token)
+
     await router.push('/story/setup')
+    return
+  } catch { }
 
-    await googleRegister({ token: idToken })
-
+  try {
     const { access_token, refresh_token } = await googleLogin({ token: idToken })
     localStorage.setItem('access_token', access_token)
     localStorage.setItem('refresh_token', refresh_token)
 
+    await router.push('/story/setup')
+    return
     // emit next or redirect
   } catch {
     await router.push('/auth')
